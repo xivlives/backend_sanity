fbde114db688ba956c9598ce27ad1d86e101db1c
favour orji <favour@learnflo.co>
2023-06-20 20:52:00 +0100

    new portfolio using sanity

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import testimonials from "./testimonials"
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [testimonials, works, brands, abouts, experiences, skills, workExperience, contact]
